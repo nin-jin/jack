218134336a493e7c798e9a1cbf7c175a5c834a61
nin-jin <sairi-na-tenshi@ya.ru>
2023-09-19 00:55:22 +0300

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,8 +1,8 @@
 export default function handler(request, response) {
   const { name = 'World' } = request.query;
   return response.send(JSON.stringify({
-            "version": request.body,//.version,
-            "session": request.body,//.session,
+            "version": request.body.version,
+            "session": request.body.session,
             "response": {
                 "text": 'Привет',
                 "end_session": false,
