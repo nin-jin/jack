ace03bec7868afd4db202ed23100ad6a008dbd57
nin-jin <sairi-na-tenshi@ya.ru>
2023-09-19 23:16:56 +0300

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,10 +1,23 @@
+let text = ''
 export default function handler(request, response) {
+
+  const command = request.body.request.command
+  let resp = ''
+
+  if( command === 'повтори' ) {
+    resp = text
+    text = ''
+  } else {
+    text += ' ' + command
+  }
+  
   return response.send({
       "version": request.body.version,
       "session": request.body.session,
       "response": {
-          "text": request.body.request.command,
+          "text": resp,
           "end_session": false,
       }
-  });
+  })
+  
 }
